def92ef9867d945ec50d5ff29adcad150d9ba2cd
Nombre <prueba@prueba.com>
2021-10-22 17:07:29 +0200

    Primer Commit.

---
+++ src/main/java/hibernate/ejercicioHibernate/App.java
@@ -0,0 +1,13 @@
+package hibernate.ejercicioHibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
